c04af42091e7495788383b49bb2cef77ff2fda19
TimusVladimir <timusvladimir@gmail.com>
2024-06-21 09:17:49 +0300

    Bubble sortirovka

--- bublesort.py
+++ bublesort.py
@@ -14,3 +14,18 @@ for i in range(n-1):
            spisok[j], spisok[j+1] = spisok[j+1], spisok[j]
 
 print(spisok)
+
+N = 10
+a = [randint(1, 99) for n in range(N)]
+print(a)
+
+i = 0
+while i < N-1:
+    j = 0
+    while j < N-1-i:
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+        j += 1
+    i += 1
+
+print(a)
